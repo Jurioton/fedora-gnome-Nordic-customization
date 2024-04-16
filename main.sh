c8dfbe9bb6d626b042638a2aafb071b657665f0b
Jurioton <167158544+Jurioton@users.noreply.github.com>
2024-04-17 01:08:14 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,31 +6,31 @@ user_name="$USER"
 echo "Cleaning directories..."
 
 # Installing gtk-murrine
-sudo dnf install gtk-murrine-engine
+#sudo dnf install gtk-murrine-engine
 
 #Cloning required repos
 #Evergreen GTK Theme and Icons
 git clone https://github.com/EliverLara/Nordic.git --depth=1
 mv Nordic .themes
 # Cursors
-git clone https://github.com/vinceliuice/WhiteSur-cursors.git --depth=1
+#git clone https://github.com/vinceliuice/WhiteSur-cursors.git --depth=1
 # Wallpapers
-git clone https://github.com/isouravgope/Nord-Wallpapers.git
-mv Nord-Wallpapers/ .local/share/backgrounds/
+#git clone https://github.com/isouravgope/Nord-Wallpapers.git
+#mv Nord-Wallpapers/ .local/share/backgrounds/
 
 
 
 #Setting up the theme Nordic theme
-gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
-gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+#gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+#gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 
 #Setting up the icons
-git clone https://github.com/alvatip/Nordzy-icon.git
-cd Nordzy-icon/
-sudo ./install.sh
+#git clone https://github.com/alvatip/Nordzy-icon.git
+#cd Nordzy-icon/
+#sudo ./install.sh
 
 #Setting wallpapers
 
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/<user_name>/.local/backgrounds/Nord-Wallpapers/wallpapers/21.png"
+#gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/<user_name>/.local/backgrounds/Nord-Wallpapers/wallpapers/21.png"
 
 
